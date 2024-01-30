4bd8fe2d848ebb7f7851d49d9a627e57ef875c9f
deividferraz <deividferraz20@gmail.com>
2024-01-30 00:59:07 -0300

    agr foi

---
+++ ContaBanco.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace meuSuperBanco
+{
+    public class ContaBanco
+    {
+        public string Numero { get; }
+        public string Dono { get; set; }
+        public decimal Saldo 
+        {
+            get
+            {
+                decimal saldo = 0;
+                foreach (var item in todasTransacoes)
+                {
+                    saldo += item.Valor;
+                }
+                return saldo;
+            }
+                
+        }
+
+        public static int numeroConta = 1234567890;
+
+        private List<Transacao> todasTransacoes = new List<Transacao>();
+
+        public ContaBanco()
+        {
+
+        }
+
+        public ContaBanco(string dono, decimal valor)
+        {
+            this.Dono = dono;
+            numeroConta++;
+
+            this.Numero = numeroConta.ToString();
+            Depositar(valor, DateTime.Now, "Saldo inicial");
+        }
+
+        public void Depositar(decimal valor, DateTime data, string obs)
+        {
+            if (valor <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(valor),
+                    "Não aceitamos depositos de valor negativo");
+            }
+            Transacao trans = new Transacao(valor, data, obs);
+            todasTransacoes.Add(trans);
+        }
+
+        public void Sacar(decimal valor, DateTime data, string obs)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor), "impossivel realizar o saque");
+            }
+            if (Saldo - valor <0)
+            {
+                throw new InvalidOperationException("Essa operação não é permitida");
+            }
+            Transacao trans = new Transacao(-valor, data, obs);
+            todasTransacoes.Add(trans);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace meuSuperBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            meuSuperBanco.ContaBanco contaB = new ContaBanco("Deivid", 1000);
+
+            Console.WriteLine($"A conta {contaB.Numero} de {contaB.Dono} foi criada com {contaB.Saldo}");
+
+            contaB.Sacar(300, DateTime.Now, "Fiz as compras");
+            Console.WriteLine($"A conta está com {contaB.Saldo}");
+
+            try
+            {
+                contaB.Sacar(100000, DateTime.Now, "Vou pagar");
+                Console.WriteLine($"A conta está com {contaB.Saldo}");
+            }catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);  
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Operação não realizada");
+            }
+
+            contaB.Sacar(1500, DateTime.Now, "Pagar a escola das crianças");
+            Console.WriteLine($"A conta está com {contaB.Saldo}");
+
+            contaB.Sacar(2000, DateTime.Now, "Alugues");
+            Console.WriteLine($"A conta está com {contaB.Saldo}");
+        }
+    }
+}
